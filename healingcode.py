a83b0241a732fa315dd1219aee849fa817617b49
sfmk07 <151550819+sfmk07@users.noreply.github.com>
2023-12-15 16:31:03 +0100

    sauvegarde2

--- healingcode.py
+++ healingcode.py
@@ -18,3 +18,23 @@ class Patient:
     def payer(self, montant):
         self.argent -= montant
         print(f"{self.nom} a payé {montant}€.")
+
+class Docteur:
+    def __init__(self):
+        self.nom = "Débogueur"
+        self.argent = 0
+        self.cabinet = ["chat"]
+        self.table_de_diagnostic = {
+            "mal indenté": "ctrl+maj+f",
+            "unsave": "saveOnFocusChange",
+            "404": "CheckLinkRelation",
+            "azmatique": "Ventoline",
+            "syntaxError": "f12+doc"
+        }
+
+    def diagnostiquer(self, patient):
+        print(f"Diagnostic pour {patient.nom}: {self.table_de_diagnostic[patient.maladie]}")
+        return self.table_de_diagnostic[patient.maladie]
+
+    def recevoir_paiement(self, montant):
+        self.argent += montant
